1c85af6c98c835c47b750178adeb6596fa1147cb
Amier Putra <86624398+amierputra@users.noreply.github.com>
2023-11-06 22:57:14 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-import fs from 'fs';
-
-const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '$': 'jQuery'
-        },
-    },
-    server: {
-        host: true,
-        port: env.VITE_ASSET_PORT,
-        strictPort: true,
-        hmr: {
-            host: env.VITE_ASSET_HOST,
-            port: env.VITE_ASSET_PORT,
-        },
-        https: {
-            key: fs.readFileSync(env.VITE_PRIVKEY_PATH),
-            cert: fs.readFileSync(env.VITE_CERT_PATH),
-        },
-    }, 
 });
